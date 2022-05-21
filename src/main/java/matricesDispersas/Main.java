8ca3a96399dbf715e9ee65311d14d9b0d534f3d9
Bryanzdruls <eltiobryanz@gmail.com>
2022-05-21 15:11:03 -0500

    Arreglar crear

--- src/main/java/matricesDispersas/Main.java
+++ src/main/java/matricesDispersas/Main.java
@@ -19,58 +19,23 @@ public class Main {
         //Instancias de objetos
         tripleta t1;
         tripleta t2;
-        tripleta t3;
-        
-        int opc= 0;//Variable de opcion
-        Random aleatorio=new Random();//Random god
-        int mat[][]=new int[3][3], sw=0,Dif0=0;
-        int n = mat.length, m = mat[0].length; //NxM se definen              
-        for (int i = 0; i < mat.length; i++)//Llenar de 0.
-        {
-            for (int j = 0; j < mat[0].length; j++) {
-                mat[i][j]= 0;
-            }
-        }//fin de llenar de 0.
-        System.out.println("Matriz con 0: \n");
-        for (int i = 0; i < mat.length; i++)//Mostrar 0
-        {
-            for (int j = 0; j <mat[0].length; j++) {
-                System.out.print("|"+mat[i][j]+"|");
-            }
-            System.out.println("");
-        }//Fin Mostrar 0
-        System.out.println("Matriz Dispersa: ");
-        for (int i = 0; i < n*m; i++)//Generador de numeros aleatorios
-        {
-            if (sw==0)
-            {
-                mat[aleatorio.nextInt(4-1)][aleatorio.nextInt(4-1)]=aleatorio.nextInt(99);
-                sw=1;
-                Dif0+=1;
-            }
-            else
-            {
-                if (i<(n*m)/2)
-                {
-                    sw=0;
-                }
-            }
-        }//Fin de generador de numeros aleatorios     
-        for (int i = 0; i < mat.length; i++)
-        {
-            for (int j = 0; j <mat[0].length; j++) {
-                System.out.print("|"+mat[i][j]+"|");
-            }
-            System.out.println("");
-        }// Mostrar
-        
+        tripleta t3;        
+        int opc= 0;//Variable de opcion       
+        int mat[][]=new int[3][3], sw=0,Dif0=0, swTri=0;//Matriz definida, tamaño y otros
+        int n = mat.length, m = mat[0].length; //NxM se definen          
+        Dif0=Matriz(mat,n,m, sw,Dif0);//Llamado al metodo de la matriz dispersa
+        t1= new tripleta(Dif0+1);
         do {
             opc= menu();
             switch (opc) {
                 case 1://tripleta
                     opc=menuT();
-                    t1= new tripleta(Dif0+1);
-                    t1.Crear(n,m,Dif0+1,mat);//Funciona                 
+                    if(swTri==0)
+                    {                      
+                        t1.Crear(n,m,Dif0,mat);//Funciona  
+                        swTri=1;
+                        Dif0=0;
+                    }            
                     switch (opc) {
                         case 1://Suma de filas.
                             System.out.println("Suma de filas:");
@@ -81,7 +46,7 @@ public class Main {
                             t1.sumaColumnas();
                             break;
                         case 3://Insertar Dato
-                            
+                            t1 = t1.insertarDato();
                             break;
                         case 4://Eliminar Dato
                             
@@ -106,7 +71,7 @@ public class Main {
                             }
                             else
                             {
-                                JOptionPane.showConfirmDialog(null, "La tripleta esta vacia");
+                                JOptionPane.showMessageDialog(null, "La tripleta esta vacia");
                             }
                             break;
                         case 10:
@@ -115,8 +80,7 @@ public class Main {
                         default:
                             JOptionPane.showMessageDialog(null,"La opcion ingresada es incorrecta, por favor ingrese otra opcion");
                             break;
-                    }
-                    
+                    }                    
                     opc=0;
                     break;
                     
@@ -127,7 +91,10 @@ public class Main {
                     
                     break;
                 case 4:
-                    
+                    System.out.println("MATRIZ REHECHA");
+                    Matriz(mat,n,m, sw, Dif0);
+                    t1= new tripleta(Dif0+1);
+                    swTri=0;
                     break;
                 case 5:
                     
@@ -135,14 +102,9 @@ public class Main {
                 default:
                     JOptionPane.showMessageDialog(null,"La opcion ingresada es incorrecta, por favor ingrese otra opcion");
                     break;
-            }
-        
-        } while (opc!=5);
-        
-        
-        
-        
-    }
+            }        
+        } while (opc!=5);      
+    }//Fin main
     
     public static int menu()
     {
@@ -150,7 +112,7 @@ public class Main {
         +"1. Ingresar Tripleta\n"
         +"2. Ingresar Forma 1\n"
         +"3. Ingresar Forma 2 \n"
-        +"4.Reiniciar matriz base\n"
+        +"4. Reiniciar matriz base\n"
         +"5. Salir\n"
         +"Ingrese una opcion: "));
     }
@@ -169,4 +131,78 @@ public class Main {
         +"10.Volver\n"
         +"Ingrese una opcion: "));
     }
+    public static int menuF1()
+    {
+        return Integer.parseInt(JOptionPane.showInputDialog("---MENU TRIPLETA---\n"
+        +"1. Suma de filas\n"
+        +"2. Suma de Columnas\n"
+        +"3. Insertar Dato \n"
+        +"4. Eliminar dato\n"
+        +"5. Mostrar mayor dato impar (Fila)\n"
+        +"6. Mostrar la columna con mayor promedio\n"
+        +"7. Suma de diagonal Principal\n"
+        +"8. Suma Diagonal secundaria\n"
+        +"9. Mostrar tripleta\n"
+        +"10.Volver\n"
+        +"Ingrese una opcion: "));
+    }//Fin menu f1
+    public static int menuF2()
+    {
+        return Integer.parseInt(JOptionPane.showInputDialog("---MENU TRIPLETA---\n"
+        +"1. Suma de filas\n"
+        +"2. Suma de Columnas\n"
+        +"3. Insertar Dato \n"
+        +"4. Eliminar dato\n"
+        +"5. Mostrar mayor dato impar (Fila)\n"
+        +"6. Mostrar la columna con mayor promedio\n"
+        +"7. Suma de diagonal Principal\n"
+        +"8. Suma Diagonal secundaria\n"
+        +"9. Mostrar tripleta\n"
+        +"10.Volver\n"
+        +"Ingrese una opcion: "));
+    }//Fin menu F2
+    public static int Matriz(int [][]mat,int n, int m, int sw, int dif0)
+    {
+        dif0=0;
+        Random aleatorio=new Random();//Random god
+        for (int i = 0; i < mat.length; i++)//Llenar de 0.
+        {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j]= 0;
+            }
+        }//fin de llenar de 0.
+        System.out.println("Matriz con 0: \n");
+        for (int i = 0; i < mat.length; i++)//Mostrar 0
+        {
+            for (int j = 0; j <mat[0].length; j++) {
+                System.out.print("|"+mat[i][j]+"|");
+            }
+            System.out.println("");
+        }//Fin Mostrar 0
+        System.out.println("Matriz Dispersa: ");
+        for (int i = 0; i < n*m; i++)//Generador de numeros aleatorios
+        {
+            if (sw==0)
+            {
+                mat[aleatorio.nextInt(4-1)][aleatorio.nextInt(4-1)]=aleatorio.nextInt(99);
+                sw=1;
+                dif0= dif0+1;
+            }
+            else
+            {
+                if (i<(n*m)/2)
+                {
+                    sw=0;
+                }
+            }
+        }//Fin de generador de numeros aleatorios     
+        for (int i = 0; i < mat.length; i++)
+        {
+            for (int j = 0; j <mat[0].length; j++) {
+                System.out.print("|"+mat[i][j]+"|");
+            }
+            System.out.println("");
+        }// Mostrar
+        return dif0;
+    }//Fin matriz
 }//Fin main
--- src/main/java/matricesDispersas/tripleta.java
+++ src/main/java/matricesDispersas/tripleta.java
@@ -1,5 +1,7 @@
 package matricesDispersas;
 
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -36,10 +38,10 @@ public class tripleta
         tri[f][c] = d;
     }
    
-    public void Crear(int n, int m, int d, int mat[][])
+    public void Crear(int n, int m, int d, int mat[][])//Solucionar errores con datos <>
     {
         int k=1;
-        tri[0][0]=n;tri[0][1]=m;tri[0][2]=d-1;
+        tri[0][0]=n;tri[0][1]=m;tri[0][2]=d;
        
         //mat.length i, mat[i].length
         for (int i = 0; i < n; i++)
@@ -56,9 +58,9 @@ public class tripleta
     }//fin crear
     public void mostrar()
     {
-        for (int k = 0; k <= tri[0][2]; k++)
+        for (int k = 0; k <= getTri(0, 2); k++)
         {
-            System.out.println("|"+tri[k][0]+"|"+tri[k][1]+"|"+tri[k][2]+"|");
+            System.out.println("|"+getTri(k, 0)+"|"+getTri(k, 1)+"|"+getTri(k, 2)+"|");
         }
     }//Fin mostrar
     public void sumaFilas()
@@ -76,7 +78,7 @@ public class tripleta
             System.out.println("La suma de filas "+ valor+ " es: "+sumaF);       
         }
     }//Fin sumar filas.
-    public void sumaColumnas()
+    public void sumaColumnas()//Pequeñas correcciones
     {
         int sumC, k=1, valor =0;
         while (valor<=tri[0][1]) 
@@ -94,5 +96,83 @@ public class tripleta
             valor+=1;
             k=1;
         }
+    }//Fin suma columnas
+    public tripleta insertarDato()//Arreglar
+    {
+        int f, c, d, k=1,swIgual=0;
+        d = Integer.parseInt(JOptionPane.showInputDialog("Que dato desea ingresar: "));
+        f = Integer.parseInt(JOptionPane.showInputDialog("¿En que fila lo desea insertar?"));
+        if(f<getTri(0, 0)&& !(f<0))
+        {
+            c = Integer.parseInt(JOptionPane.showInputDialog("¿En que columna lo desea insertar?"));
+            if(c<getTri(0, 1)&& !(c<0))
+            {
+               while(k<=getTri(0, 2))//caso suma
+               {
+                    if (f==getTri(k, 0) && c== getTri(k, 1))
+                    {
+                        setTri(k, 2, getTri(k, 2)+d);
+                        swIgual=1;
+                    }
+                    k++;
+               } 
+               if(swIgual == 0)
+               {
+                   tripleta t1 = new tripleta(getTri(0, 2)+2);//CREAR NUEVA TRIPLETA
+                   t1.setTri(0, 2, getTri(0,2)+1);//DATOS <> 0 NUEVA TRIPLETA                                 
+                   t1 =t1.llenarTri(this);//LLENAR TRIPLETA CON DATOS ORIGINALES
+                   t1.setTri(f, 0, f);//En la posicion de la fila le ponemos el dato de la fila
+                   t1.setTri(f, 1, c);//En la posicion de la columna le ponemos el dato de la columna.
+                   t1.setTri(f, 2, d);//En la posicion respectiva le ponemos el dato.  
+                   return t1;
+               }
+            }
+            else
+            {
+                if(c>getTri(0, 1))
+                {
+                     JOptionPane.showMessageDialog(null, "La fila insertada es mayor que la cantidad de filas en la tripleta.");
+                }
+                else if(c<0)
+                {
+                    JOptionPane.showMessageDialog(null, "No existen filas menores que 0.");
+                }
+            }//Fin condicion tamaño de C           
+        }
+        else
+        {
+            if(f>getTri(0, 0))
+            {
+                JOptionPane.showMessageDialog(null, "La fila insertada es mayor que la cantidad de filas en la tripleta.");
+            }
+            else if(f<0)
+            {
+                JOptionPane.showMessageDialog(null, "No existen filas menores que 0.");
+            }
+        }//Fin condicion tamaño de f.  
+        return this;
+    }//Fin insertar
+    public tripleta llenarTri(tripleta t)//Arreglar
+    {
+        int i=1, j=1;
+        setTri(0, 0, t.getTri(0, 0));
+        setTri(0, 1, t.getTri(0, 1));
+        while (i<this.getTri(0, 2))
+        {       
+            if(getTri(i, 2)==0)
+            {
+               setTri(i, 0, t.getTri(j, 0));
+               setTri(i, 1, t.getTri(j, 1));
+               setTri(i, 2, t.getTri(j, 2));
+               i++;
+               j++;
+            }
+            else
+            {
+              i++;  
+            }
+             
+        }
+        return this;
     }
 }//Fin class
